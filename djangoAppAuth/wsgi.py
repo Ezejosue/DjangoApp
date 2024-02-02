f562c1ed541c7c7ceb6e607685ad113b5440a907
Josué Ávalos <aezequiel56@gmail.com>
2024-02-02 16:35:01 -0600

    Initial settings

---
+++ djangoAppAuth/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for djangoAppAuth project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoAppAuth.settings')
+
+application = get_wsgi_application()
