80765277a8351ccef5d311a5bf5365504e8d6a01
Josué Ávalos <aezequiel56@gmail.com>
2024-02-04 16:32:42 -0600

    Enhancements in views.py and creation of base.html in templates folder
    The exception in views.py was modified to be more specific with an IntegrityError. In the templates folder, a base.html was created to establish the foundation of the code that will be repeated on all other pages, where for now, the navbar is located. It was also added that after registering a new user, a cookie with the session ID is stored.

--- djangoAppAuth/urls.py
+++ djangoAppAuth/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('task/', views.tasks, name='task'),
 
 ]
