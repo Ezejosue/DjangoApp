28c68448e83dfb70a68f65fa7a810874c6353ddd
Josué Ávalos <aezequiel56@gmail.com>
2024-02-04 17:24:06 -0600

    Creation of the task model
    Creation of the task model, database migration was performed, and the admin control panel was configured through the superuser to display this model.

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+# Task Model Admin
+admin.site.register(Task, TaskAdmin)
