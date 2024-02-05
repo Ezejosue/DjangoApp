1d0551c8233c1b8a00ee6306830d250dc5d81ae4
Josué Ávalos <aezequiel56@gmail.com>
2024-02-05 11:07:26 -0600

    Bootstrap 5 design
    Add basic Bootstrap 5 design to the various HTML templates in the 'templates' folder.

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,11 +1,15 @@
-from django.forms import ModelForm
-
+from django import forms
 from .models import Task
 
 # Task Create Form
 
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        }
