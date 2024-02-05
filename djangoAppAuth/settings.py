cf887c2c738af659d14aa669daec694ac9723d80
Josué Ávalos <aezequiel56@gmail.com>
2024-02-05 12:18:34 -0600

    Update settings.py

--- djangoAppAuth/settings.py
+++ djangoAppAuth/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'djangoAppAuth.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresq1://postgres:postgres@localhost/postgres', conn_max_age=600)
+    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost/postgres', conn_max_age=600)
     # 'ENGINE': 'django.db.backends.sqlite3',
     # 'NAME': BASE_DIR / 'db.sqlite3',
 
